a26b3f6597c5a256d8887a6b4af34af781295f60
pbalapra <pbalapra@anl.gov>
2018-03-23 01:29:24 +0000

    minor changes

--- workflows/common/sh/sched-theta.sh
+++ workflows/common/sh/sched-theta.sh
@@ -4,7 +4,8 @@
 
 # Tell Swift/T to generate and submit a Theta-formatted script
 #export QUEUE=${QUEUE:-debug-flat-quad}
-export QUEUE=${QUEUE:-default}
+export QUEUE=${QUEUE:-CSC249ADOA01_walltime}
 export PROJECT=CSC249ADOA01
+#export PROJECT=CSC249ADOA01_walltime
 MACHINE="-m theta"
 
